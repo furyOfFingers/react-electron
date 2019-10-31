302fddea1f4b63dd7199bdf718669d29f0b96833
furyOfFingers <maratformusic@gmail.com>
2019-10-31 21:45:30 +0300

    change format, added style for component Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './style.css';
+
+/**
+ * Свойства компонента Button.
+ */
+interface IProps {
+/** Текст Button */
+text?: string;
+/** Блокировка Button */
+disabled?: boolean;
+/** Button onClick function */
+onClick?: () => void;
+/** Наименование иконки */
+iconName?: string | null;
+}
+
+/**
+ * Компонент Button.
+ */
+const Button = ({
+  text,
+  disabled,
+  onClick,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text && <span>{text}</span>}
+    </button>
+  );
+};
+
+export default Button;
